3d6fd713153d6f4c92db06fd198b610edb9243e5
hnjxzby <hnjxzby@hnpa.edu.cn>
2023-02-03 15:42:37 +0800

    2023-2-3modify

--- untitled1-17flower-GAP.py
+++ untitled1-17flower-GAP.py
@@ -1,17 +1,20 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Feb  8 13:48:12 2022
-
+hnjxzby@2023-2-3重新测试通过
 @author: Dell_hnjxzby
 """
 
-import tensorflow.keras
+#import tensorflow.keras
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import Model
 from tensorflow.keras.applications.vgg19 import VGG19
 from tensorflow.keras.layers import Dense, Flatten
 from matplotlib import pyplot as plt
 import numpy as np
+
+from tensorflow import optimizers
+
 
 # 为保证公平起见，使用相同的随机种子
 np.random.seed(7)
@@ -26,8 +29,8 @@ NUM_CLASSES = 17
 #TRAIN_PATH = '/home/yourname/Documents/tensorflow/images/17flowerclasses/train'
 #TEST_PATH = '/home/yourname/Documents/tensorflow/images/17flowerclasses/test'
 
-TRAIN_PATH = 'D:\zbytest\17flowerclasses/train'
-TEST_PATH = 'D:\zbytest\17flowerclasses/test'
+TRAIN_PATH = 'D:/zbytest/17flowerclasses/train'
+TEST_PATH = 'D:/zbytest/17flowerclasses/test'
 
 
 FLOWER_CLASSES = ['Bluebell', 'ButterCup', 'ColtsFoot', 'Cowslip', 'Crocus', 'Daffodil', 'Daisy',
@@ -53,14 +56,14 @@ def model(mode='fc'):
         base_model = VGG19(input_shape=(IMAGE_SIZE, IMAGE_SIZE, 3), include_top=False, pooling='max')
         x = base_model.output
         prediction = Dense(NUM_CLASSES, activation='softmax')(x)
-
-    model = Model(input=base_model.input, output=prediction)
+    #model = Model(input=base_model.input, output=prediction)  ##很有意思，要在单词input和output之后加上s才能正确运行 hnjxzby@2023-2-3
+    model = Model(inputs=base_model.input, outputs=prediction)
     model.summary()
-    opt = keras.optimizers.rmsprop(lr=0.0001, decay=1e-6)
+    opt = optimizers.RMSprop(lr=0.0001, decay=1e-6)
+    #opt = keras.optimizers.RMSprop(lr=0.0001,decay=1e-6) ###用keras的优化器不行，要修改成tensorflow的才行！！！今天修改了这两个地方程序运行通过。
     model.compile(loss='categorical_crossentropy',
                              optimizer=opt,
                              metrics=['accuracy'])
-
     # 使用数据增强
     train_datagen = ImageDataGenerator()
     train_generator = train_datagen.flow_from_directory(directory=TRAIN_PATH,
@@ -73,7 +76,6 @@ def model(mode='fc'):
     # 运行模型
     history = model.fit_generator(train_generator, epochs=epochs, validation_data=test_generator)
     return history
-
 
 fc_history = model('fc')
 avg_history = model('avg')
